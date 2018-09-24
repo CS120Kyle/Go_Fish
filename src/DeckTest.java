02435f1e190cada598fcdeefbdf744f86d1835de
Andrew Nolin <asnolin@uvm.edu>
2018-09-24 12:01:10 -0400

    Deck deals hands

--- src/DeckTest.java
+++ src/DeckTest.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class DeckTest{
 	public static void main(String[] args){
@@ -10,5 +10,15 @@ public class DeckTest{
 		myDeck.shuffle();
 		System.out.println("order after string:");
 		myDeck.printDeck();
+
+		//test dealHands and pickup
+		LinkedList<Integer> hand1 = new LinkedList<Integer>();
+		LinkedList<Integer> hand2 = new LinkedList<Integer>();
+		System.out.println("hand1 before dealHands:" + hand1.toString());
+		System.out.println("hand2 before dealHands:" + hand2.toString());
+		myDeck.dealHands(hand1, hand2);
+		System.out.println("hand1 after dealHands:" + hand1.toString());
+		System.out.println("hand2 after dealHands:" + hand2.toString());
+		System.out.println(myDeck.numCards());
 	}//end main
 }//end class DeckTest
