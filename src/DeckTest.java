22956a13b74a388f8601f0c86f63a5ae3cac4df2
Andrew Nolin <asnolin@uvm.edu>
2018-09-24 01:31:11 -0400

    Deck returns right number of cards

--- src/DeckTest.java
+++ src/DeckTest.java
@@ -3,6 +3,8 @@
 public class DeckTest{
 	public static void main(String[] args){
 		Deck myDeck = new Deck();
+		int cards = myDeck.numCards();
+		System.out.println("number of cards:"+cards);
 		System.out.println("whoop");
 	}//end main
 }//end class DeckTest
